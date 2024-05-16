a3713f7fb3a2fb4bd393aab1015e3c7967a2e51e
if001 <otomijuf.004@gmail.com>
2024-05-16 23:15:33 +0900

    fix: args

--- merge.py
+++ merge.py
@@ -4,7 +4,7 @@
 
 import argparse
 import yaml
-from dataclasses import dataclass, asdict
+from dataclasses import dataclass, asdict, field
 
 import torch
 from mergoo.compose_experts import ComposeExperts
@@ -13,7 +13,6 @@ def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("--config_path", type=str, required=True)
     parser.add_argument("--output_path", type=str, required=True)
-    parser.add_argument("--tokenizer", type=str, default="team-sanai/unigram_32000")
     # parser.add_argument("--repo_id", type=str)
     # parser.add_argument("--wandb", type=str, required=True)
     args = parser.parse_args()
@@ -22,7 +21,7 @@ def parse_arguments():
 
 @dataclass
 class ExpertConfig:
-    exprt_name: str
+    expert_name: str
     model_id: str
 
 @dataclass
@@ -30,8 +29,9 @@ class Config:
     model_type: str
     num_experts_per_tok: int
     base_model: list
+    tokenizer: str = "team-sanai/unigram_32000"
     virtual_expert: bool = False
-    experts: list[ExpertConfig]
+    experts: list[ExpertConfig] = field(default_factory=list)
 
     @classmethod
     def load(cls, filename: str):
@@ -42,11 +42,11 @@ class Config:
 def main():
     args = parse_arguments()
     config = Config.load(args.config_path)
-    config = asdict(config)
+    config_dict = asdict(config)
 
-    expertmerger = ComposeExperts(config, torch_dtype=torch.bfloat16)
+    expertmerger = ComposeExperts(config_dict, torch_dtype=torch.bfloat16)
     expertmerger.compose()
-    expertmerger.save_checkpoint(args.output_path, args.tokenizer)
+    expertmerger.save_checkpoint(args.output_path, config.tokenizer)
 
     # tokenizer = AutoTokenizer.from_pretrained("./"+model_id)
     # model = AutoModelForCausalLM.from_pretrained("./"+model_id)
