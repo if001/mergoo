ef489b0294d08c3ecf1e34517024388fc5553fb1
if001 <otomijuf.004@gmail.com>
2024-05-21 23:38:55 +0900

    fix infarence

--- scripts/infarence.py
+++ scripts/infarence.py
@@ -9,7 +9,7 @@ from transformers import (
     GenerationConfig,
     AutoModelForCausalLM as AutoModelForCausalLMHF
 )
-
+from datasets import load_dataset
 from mergoo.models.modeling_llama import LlamaForCausalLM
 
 
@@ -25,16 +25,41 @@ def parse_arguments():
     print("args: ", args)
     return args
 
-def load_prompt(prompt_file = None, prompt = None):
-    if prompt:
-        return [prompt]
-    li = []
-    with open(prompt_file) as f:
-        li = f.readlines()
-    li2 = []
-    for l in li:
-        li2.append(l.strip())
-    return li2
+def gen(model, tokenizer, generationConfig, prompt, ans=None, include_ans=False):
+    print("input: ", prompt)
+    encoded_text = tokenizer.tokenize(prompt)
+    print("encode", len(encoded_text), encoded_text)
+
+    input_ids = tokenizer(prompt, return_tensors="pt", add_special_tokens=False).input_ids
+    print("input_ids", input_ids)
+
+    model_outputs = model.generate(
+                input_ids=input_ids,
+                generation_config=generationConfig
+    )
+    model_text_output = tokenizer.decode(model_outputs[0], skip_special_tokens=True)
+    print("output: ", model_text_output)
+    if include_ans:
+        if ans in model_text_output:
+            print("success")
+        else:
+            print("fail")
+    print("="*50)
+
+
+def check_next_prob(model, tokenizer, prompt):
+    input_ids = tokenizer(prompt, return_tensors="pt", add_special_tokens=False).input_ids
+    outputs = model(input_ids=input_ids)
+    logits = outputs.logits[0, -1, :]
+    probabilities = torch.softmax(logits, dim=-1)
+
+    ## 上位5つのトークンの確率とトークン自体を取得
+    top_5_tokens = torch.topk(probabilities, 10)
+    ## それぞれのトークンとその確率を表示
+    for i, token_id in enumerate(top_5_tokens.indices):
+        token = tokenizer.decode([token_id])
+        print(f"Rank {i+1}: Token = {token}, Probability = {top_5_tokens.values[i]:.4f}")
+
 
 def main():
     args = parse_arguments()
@@ -46,51 +71,22 @@ def main():
         torch_dtype=torch.float16,
     )
 
-    #for name, weight in model.named_parameters():
-    #    print(name, weight[:10])
-
-    ht_model = AutoModelForCausalLMHF.from_pretrained(
+    tanuki_model = AutoModelForCausalLMHF.from_pretrained(
         "hatakeyama-llm-team/Tanuki_pretrained_stage6_step62160",
         torch_dtype=torch.float16,
     )
+    generationConfig = GenerationConfig(do_sample=True, repetition_penalty=1.1, temperature=0.2, max_new_tokens=30)
 
-    prompt = load_prompt(prompt_file=args.prompt_file, prompt=args.prompt)
-    for v in prompt:
-        print("input: ", v)
-        input_ids = tokenizer(v, return_tensors="pt", add_special_tokens=False).input_ids
-        
-        #outputs = model(input_ids=input_ids)
-        #logits = outputs.logits[0, -1, :]
-        #probabilities = torch.softmax(logits, dim=-1)
+    if args.prompt:
+        gen(model, tokenizer, generationConfig, args.prompt)
+        gen(tanuki_model, tokenizer, generationConfig, args.prompt)
 
-        # 上位5つのトークンの確率とトークン自体を取得
-        #top_5_tokens = torch.topk(probabilities, 10)
-        # それぞれのトークンとその確率を表示
-        #for i, token_id in enumerate(top_5_tokens.indices):
-        #    token = tokenizer.decode([token_id])
-        #    print(f"Rank {i+1}: Token = {token}, Probability = {top_5_tokens.values[i]:.4f}")
-        #exit()
+    
+    dataset = load_dataset("csv", data_files=args.prompt_file, split="train")
+    for v in dataset:
+        include_ans = int(v["include_ans"]) == 1
+        gen(model, tokenizer, generationConfig, v["text"], v["ans"], include_ans)
+        gen(tanuki_model, tokenizer, generationConfig, v["text"], v["ans"], include_ans)
 
-        generationConfig = GenerationConfig(do_sample=True, repetition_penalty=1.1, temperature=0.2, max_new_tokens=30)
-
-        peft_model_outputs = model.generate(
-                input_ids=input_ids,
-                generation_config=generationConfig
-        )
-        peft_model_text_output = tokenizer.decode(peft_model_outputs[0], skip_special_tokens=True)
-        _t = tokenizer.tokenize(v)
-        print("encode", len(_t), _t)
-        print("input_ids", input_ids)
-        print("output: ", peft_model_text_output)
-        print("="*100)
-
-        ht_model_outputs = ht_model.generate(
-            input_ids=input_ids, 
-            generation_config=generationConfig
-        )
-        ht_model_text_output = tokenizer.decode(ht_model_outputs[0], skip_special_tokens=True)
-        print("ht output: ", ht_model_text_output)
-        print("="*100)
-        print("*"*200)
 if __name__ == "__main__":
     main()
