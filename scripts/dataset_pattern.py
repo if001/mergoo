096cba5e08c7d8b0c5f4f0c648ef76ce7d385862
if001 <otomijuf.004@gmail.com>
2024-05-23 00:02:14 +0900

    fix: add pattern

--- scripts/dataset_pattern.py
+++ scripts/dataset_pattern.py
@@ -81,3 +81,95 @@ def get_dataset_pattern(pattern_name):
             "dentaku": "/storage6/dataset/pretrain/gen_experet/dentaku/train_data_dentaku_2keta_only_add_1_3keta.jsonl",
         }
         return ratios, target_list
+
+    if pattern_name == "pattern6":
+        ratios = {
+           "wiki_ja": 0.01,
+           "basic_math_dentaku": 0.01,
+        }
+        target_list = {
+            "wiki_ja": "/storage6/dataset/pretrain/gen_experet/WIKI/raw/wikipedia_ja/merged_wikipedia_ja_16.0.jsonl",
+            "basic_math_dentaku": "/storage6/fujisawa/add_ja_3x3_delete_5_pattern.jsonl",
+        }
+        return ratios, target_list
+
+    ## 5/22
+    if pattern_name == "pattern7":
+        ratios = {
+           "wiki_ja": 0.1,
+           "dentaku": 2.0,
+           "basic_math_dentaku": 0.12,
+        }
+        target_list = {
+            "wiki_ja": "/storage6/dataset/pretrain/gen_experet/WIKI/raw/wikipedia_ja/merged_wikipedia_ja_16.0.jsonl",
+            "dentaku": "/storage6/dataset/pretrain/gen_experet/dentaku/train_data_dentaku_2keta_only_add_1_3keta.jsonl",
+            "basic_math_dentaku": "/storage6/fujisawa/add_ja_3x3_delete_5_pattern.jsonl",
+        }
+        return ratios, target_list
+    
+    if pattern_name == "pattern8":
+        ratios = {
+           "wiki_ja": 0.067,
+           "dentaku": 2.0,
+           "basic_math_dentaku": 0.18,
+        }
+        target_list = {
+            "wiki_ja": "/storage6/dataset/pretrain/gen_experet/WIKI/raw/wikipedia_ja/merged_wikipedia_ja_16.0.jsonl",
+            "dentaku": "/storage6/dataset/pretrain/gen_experet/dentaku/train_data_dentaku_2keta_only_add_1_3keta.jsonl",
+            "basic_math_dentaku": "/storage6/fujisawa/add_ja_3x3_delete_5_pattern.jsonl",
+        }
+        return ratios, target_list
+    
+    if pattern_name == "pattern9":
+        ratios = {
+           "wiki_ja": 0.033,
+           "dentaku": 2.00,
+           "basic_math_dentaku": 0.24,
+        }
+        target_list = {
+            "wiki_ja": "/storage6/dataset/pretrain/gen_experet/WIKI/raw/wikipedia_ja/merged_wikipedia_ja_16.0.jsonl",
+            "dentaku": "/storage6/dataset/pretrain/gen_experet/dentaku/train_data_dentaku_2keta_only_add_1_3keta.jsonl",
+            "basic_math_dentaku": "/storage6/fujisawa/add_ja_3x3_delete_5_pattern.jsonl",
+        }
+        return ratios, target_list
+    
+    if pattern_name == "pattern10":
+        ratios = {
+           "wiki_ja": 0.1,
+           "dentaku": 10.0,
+           "basic_math_dentaku": 0.12,
+        }
+        target_list = {
+            "wiki_ja": "/storage6/dataset/pretrain/gen_experet/WIKI/raw/wikipedia_ja/merged_wikipedia_ja_16.0.jsonl",
+            "dentaku": "/storage6/dataset/pretrain/gen_experet/dentaku/train_data_dentaku_2keta_only_add_1_3keta.jsonl",
+            "basic_math_dentaku": "/storage6/fujisawa/add_ja_3x3_delete_5_pattern.jsonl",
+        }
+        return ratios, target_list
+
+    if pattern_name == "pattern11":
+        ratios = {
+           "wiki_ja": 0.066,
+           "dentaku": 20.00,
+           "basic_math_dentaku": 0.05,
+        }
+        target_list = {
+            "wiki_ja": "/storage6/dataset/pretrain/gen_experet/WIKI/raw/wikipedia_ja/merged_wikipedia_ja_16.0.jsonl",
+            "dentaku": "/storage6/dataset/pretrain/gen_experet/dentaku/train_data_dentaku_2keta_only_add_1_3keta.jsonl",
+            "basic_math_dentaku": "/storage6/fujisawa/add_ja_3x3_delete_5_pattern.jsonl",
+        }
+        return ratios, target_list
+
+    if pattern_name == "pattern12":
+        ratios = {
+           "wiki_ja": 0.033,
+           "dentaku": 2.00,
+           "basic_math_dentaku": 0.180,
+            "aozora": 0.3,
+        }
+        target_list = {
+            "wiki_ja": "/storage6/dataset/pretrain/gen_experet/WIKI/raw/wikipedia_ja/merged_wikipedia_ja_16.0.jsonl",
+            "dentaku": "/storage6/dataset/pretrain/gen_experet/dentaku/train_data_dentaku_2keta_only_add_1_3keta.jsonl",
+            "basic_math_dentaku": "/storage6/fujisawa/add_ja_3x3_delete_5_pattern.jsonl",
+            "aozora": "/storage6/corpus/category/BOOK/raw/JA/aozora/ja_book.jsonl",
+        }
+        return ratios, target_list
